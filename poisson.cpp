5ef20f020b85fd069fa557b16c597cbedfac4273
Asif <klmktu.asif@gmail.com>
2021-02-01 11:35:21 +0530

    Fix issue with generating same number when calling the function

--- poisson.cpp
+++ poisson.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 #include "poisson.h"
 
+std::random_device rd;
+std::mt19937 gen(rd());
+
 luint poisson(luint lambda)
 {
-    std::random_device rd;
-    std::mt19937 gen(rd());
     std::poisson_distribution<luint> d(lambda);
     return d(gen);
 }
